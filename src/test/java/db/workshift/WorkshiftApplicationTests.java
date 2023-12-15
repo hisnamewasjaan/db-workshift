a492b7844db6134e64b2ae7a6f2fa78b8ca29a29
Jan Bentzen <hisnamewasjaan@gmail.com>
2023-12-15 12:35:17 +0100

    init

---
+++ src/test/java/db/workshift/WorkshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package db.workshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
